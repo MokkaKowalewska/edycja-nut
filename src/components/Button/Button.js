b10d33d48a1d48150f703bb994591b2c84d83d31
Mokka Kowalewska <kowalewska.mnk@gmail.com>
2020-05-17 19:16:18 +0200

    styles: add Button component

---
+++ src/components/Button/Button.js
@@ -0,0 +1,12 @@
+import React from "react"
+import styled from "styled-components"
+
+const Button = styled.button`
+  padding: 15px;
+  border-radius: 20px;
+  background: transparent;
+  border: 2px solid #2a3747;
+  color: #2a3747;
+`
+
+export default Button
