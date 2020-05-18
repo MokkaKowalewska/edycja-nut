96afae0f4967b3de466020612f153443becd2884
Mokka Kowalewska <kowalewska.mnk@gmail.com>
2020-05-18 21:48:43 +0200

    dist: add gatsby-background-image

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,7 +20,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
-        fonts: [`Raleway`, `Syncopate`],
+        fonts: [`Jost`, `Righteous`],
         display: "swap",
       },
     },
