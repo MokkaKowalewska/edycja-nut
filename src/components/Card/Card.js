1515c3fc373e34ffdc8d31560d6031bf324926d5
Mokka Kowalewska <kowalewska.mnk@gmail.com>
2020-05-20 23:16:36 +0200

    feat: init Card component

---
+++ src/components/Card/Card.js
@@ -0,0 +1,14 @@
+import React from "react"
+import styled from "styled-components"
+
+const CardWrapper = styled.div`
+  height: 200px;
+  width: 100px;
+  border: 1px solid grey;
+`
+
+const Card = ({}) => <CardWrapper></CardWrapper>
+
+Card.propTypes = {}
+
+export default Card
