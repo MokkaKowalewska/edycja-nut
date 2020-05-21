19d0c702599302669639fccf642ac0a16dc4c756
Mokka Kowalewska <kowalewska.mnk@gmail.com>
2020-05-21 10:18:36 +0200

    feat: add 3 Cards (hardcoded)

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,12 +2,31 @@ import React from "react"
 import styled from "styled-components"
 
 const CardWrapper = styled.div`
-  height: 200px;
-  width: 100px;
-  border: 1px solid grey;
+  height: 60vh;
+  width: 25vw;
+  margin: 20px;
+  padding: 20px;
+  border-radius: 30px;
+  box-shadow: 0 20px 40px -5px lightgrey;
+  color: black;
+  text-align: center;
 `
 
-const Card = ({}) => <CardWrapper></CardWrapper>
+const Emoji = styled.figure`
+  margin: 0;
+  font-size: 10rem;
+`
+
+const Card = ({}) => (
+  <CardWrapper>
+    <Emoji>🎹</Emoji>
+    <h3>Przepisywanie nut</h3>
+    <p>
+      Możesz zlecić mi przepisywanie nut. Generalnie nie wiem dokładnie, o co
+      pięć, ale zrobię po prostu nutki jak ta lala.
+    </p>
+  </CardWrapper>
+)
 
 Card.propTypes = {}
 
--- src/pages/oferta.js
+++ src/pages/oferta.js
@@ -1,5 +1,22 @@
 import React from "react"
+import styled from "styled-components"
+import Card from "../components/Card/Card"
 
-const OfertaPage = () => <h1>Oferta</h1>
+const PageWrapper = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template: 1fr / repeat(3, 1fr);
+  justify-content: center;
+  justify-items: center;
+  gap: 20px;
+`
+
+const OfertaPage = () => (
+  <PageWrapper>
+    <Card />
+    <Card />
+    <Card />
+  </PageWrapper>
+)
 
 export default OfertaPage
