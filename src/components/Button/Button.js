7e3015af39b0753a18fa2966fb3e9178b605d2db
Mokka Kowalewska <kowalewska.mnk@gmail.com>
2020-05-23 15:53:41 +0200

    feat: adding conditional rendering, add proptypes

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,18 +1,26 @@
 import React from "react"
 import styled from "styled-components"
 import PropTypes from "prop-types"
+import { Link } from "gatsby"
 
-const ButtonStyled = styled.button`
-  padding: 15px;
-  border-radius: 20px;
-  background: transparent;
-  border: 2px solid #ffffff;
-`
+const Button = ({ children, href }) => {
+  const ButtonTag = href ? "a" : Link
 
-const Button = ({ children }) => <ButtonStyled>{children}</ButtonStyled>
+  const ButtonStyled = styled(ButtonTag)`
+    padding: 15px;
+    border-radius: 20px;
+    background: transparent;
+    border: 2px solid #ffffff;
+    color: #ffffff;
+    text-decoration: none;
+  `
+
+  return <ButtonStyled>{children}</ButtonStyled>
+}
 
 Button.propTypes = {
-  children: PropTypes.node,
+  children: PropTypes.string,
+  href: PropTypes.string,
 }
 
 export default Button
