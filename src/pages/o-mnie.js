f5d309cf7bb50733f67ae1e958841b642a2251d1
Mokka Kowalewska <kowalewska.mnk@gmail.com>
2020-05-23 15:22:42 +0200

    feat: add sourcing from WP, add map to display paragraphs

--- src/pages/o-mnie.js
+++ src/pages/o-mnie.js
@@ -6,14 +6,23 @@ import Image from "gatsby-image"
 import PageHeading from "../components/PageHeading/PageHeading"
 
 const ContentWrapper = styled.div`
-  width: 80%;
+  width: 90%;
   margin: 30px auto;
   display: flex;
   align-items: center;
 `
 
+const TextWrapper = styled.div`
+  margin-right: 40px;
+
+  h3 {
+    text-align: center;
+    font-weight: bold;
+  }
+`
+
 const BioParagraph = styled.p`
-  margin: 30px;
+  margin: 30px 0;
 `
 
 const BioImage = styled.img`
@@ -22,22 +31,51 @@ const BioImage = styled.img`
 
 const OMnie = ({ data }) => (
   <>
-    <PageHeading>{data.wordpressPage.acf.bioHeading}</PageHeading>
+    <PageHeading>{data.wordpress.pageBy.bioFields.bioheading}</PageHeading>
     <ContentWrapper>
-      <BioParagraph>{data.wordpressPage.acf.bioParagraph}</BioParagraph>
-      <BioImage src={data.wordpressPage.acf.bioImage.source_url} />
+      <TextWrapper>
+        <h3>{data.wordpress.pageBy.bioFields.bioname}</h3>
+
+        {Object.values(data.wordpress.pageBy.bioFields.bioparagraphgroup).map(
+          (bioParagraph, index) => {
+            return <BioParagraph key={index}>{bioParagraph}</BioParagraph>
+          }
+        )}
+      </TextWrapper>
+      <BioImage src={data.wordpress.pageBy.bioFields.bioimage.sourceUrl} />
     </ContentWrapper>
   </>
 )
 
+// I didn't want to spend money on Repeater field, that's why...
 export const query = graphql`
-  query bioQuery {
-    wordpressPage {
-      acf {
-        bioHeading
-        bioParagraph
-        bioImage {
-          source_url
+  {
+    wordpress {
+      pageBy(uri: "o-mnie") {
+        bioFields {
+          bioheading
+          bioname
+          bioparagraphgroup {
+            bioparagraph0
+            bioparagraph1
+            bioparagraph2
+            bioparagraph3
+            bioparagraph4
+            bioparagraph5
+            bioparagraph6
+            bioparagraph7
+            bioparagraph8
+          }
+          bioimage {
+            sourceUrl
+            sourceUrlSharp {
+              childImageSharp {
+                fluid {
+                  srcSet
+                }
+              }
+            }
+          }
         }
       }
     }
