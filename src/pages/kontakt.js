2dae7fa5109f25257ae05b26ed60cf8b1463d88c
Mokka Kowalewska <kowalewska.mnk@gmail.com>
2020-05-24 00:43:41 +0200

    style: add layout styles

--- src/pages/kontakt.js
+++ src/pages/kontakt.js
@@ -1,17 +1,30 @@
 import React from "react"
+import styled from "styled-components"
 
 import PageHeading from "../components/PageHeading/PageHeading"
 import Button from "../components/Button/Button"
 
+const ContentWrapper = styled.div`
+  text-align: center;
+`
+
+const MailButton = styled(Button)`
+  margin: 40px;
+`
+
 const Kontakt = () => (
   <>
     <PageHeading>Kto pyta, nie błądzi</PageHeading>
-    <p>
-      Każda partytura jest inna, każdy utwór to osobna historia. Pytaj, wycenię
-      pracę jak najszybciej!
-    </p>
+    <ContentWrapper>
+      <p>
+        Każda partytura jest inna, każdy utwór to osobna historia. Pytaj,
+        wycenię pracę jak najszybciej!
+      </p>
 
-    <Button to="mailto:woj.kowalewski@gmail.com">Zapytaj o stawki</Button>
+      <MailButton tag="a" href="mailto:woj.kowalewski@gmail.com">
+        Zapytaj o stawki
+      </MailButton>
+    </ContentWrapper>
   </>
 )
 
