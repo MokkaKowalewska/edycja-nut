b5554544b882c85cc0cf11c848b293213c4d71a1
Mokka Kowalewska <kowalewska.mnk@gmail.com>
2020-05-26 14:29:16 +0200

    refactor: add state, adjust styles

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,36 +1,38 @@
-import React from "react"
+import React, { useState } from "react"
 import PropTypes from "prop-types"
 import { Match } from "@reach/router"
-import styled from "styled-components"
+import styled, { css } from "styled-components"
 
 import GlobalStyle from "../assets/styles/globalStyles"
 import Navigation from "../components/Navigation/Navigation"
+import Logo from "../components/Logo/Logo"
 
 const PageWrapper = styled.div`
   width: 100%;
-  padding: 80px 50px 0;
+  ${({ color }) => color === "#000000" && `padding: 80px 50px 0;`}
 `
 
-const MainLayout = ({ children }) => (
-  <>
-    <GlobalStyle />
-    <Match path="/">
-      {props =>
-        props.match ? (
-          <>
-            <Navigation color={"#ffffff"} />
-            {children}
-          </>
-        ) : (
-          <>
-            <Navigation color={"#000000"} />
-            <PageWrapper>{children}</PageWrapper>
-          </>
-        )
-      }
-    </Match>
-  </>
-)
+const MainLayout = ({ children }) => {
+  const [color, changeColor] = useState("#ffffff")
+
+  return (
+    <>
+      <GlobalStyle />
+      <Match path="/">
+        {props => {
+          if (props.match) {
+            changeColor("#ffffff")
+          } else {
+            changeColor("#000000")
+          }
+        }}
+      </Match>
+      <Logo />
+      <Navigation color={color} />
+      <PageWrapper color={color}>{children}</PageWrapper>
+    </>
+  )
+}
 
 MainLayout.propTypes = {
   children: PropTypes.node,
