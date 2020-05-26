5e6b679858292230af8d818ff7495561b7bf7cef
Mokka Kowalewska <kowalewska.mnk@gmail.com>
2020-05-26 13:16:55 +0200

    styles: add media queries - sizes

---
+++ src/utils/media.js
@@ -0,0 +1,16 @@
+import { css } from "styled-components"
+
+const sizes = {
+  phone: 450,
+  tablet: 768,
+  desktop: 1024,
+}
+
+export default Object.keys(sizes).reduce((acc, px) => {
+  acc[px] = (...args) => css`
+    @media (max-width: ${sizes[px]}px) {
+      ${css(...args)};
+    }
+  `
+  return acc
+}, {})
